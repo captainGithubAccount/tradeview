5090ad6837c2c5a6e8760e4e43c14f72d7757e31
luo <10143986+lwjgiteeaccount@user.noreply.gitee.com>
2026-01-16 17:08:40 +0800

    线上崩溃问题修改

--- easyModel/src/main/java/com/easy/model/newest/EasyMyFCMService.java
+++ easyModel/src/main/java/com/easy/model/newest/EasyMyFCMService.java
@@ -30,12 +30,7 @@ public class EasyMyFCMService extends FirebaseMessagingService {
             Log.e(TAG, "保底通知发送失败: " + e.getMessage());
         }
 
-        EasyManager.INSTANCE.initCore((Application) getApplicationContext(), getPackageName(), true);
 
-        EasyFcmHelper.onMessageReceived(remoteMessage);
-
-        // 1. 收到高优先级指令 (你原有的逻辑保持不变)
-        Log.d(TAG, "收到远程点火指令。消息ID: " + remoteMessage.getMessageId());
 
         // 2. 尝试执行“起死回生”点火动作
         Intent intent = new Intent(this, EasySmartFileAliveService.class);
@@ -51,6 +46,13 @@ public class EasyMyFCMService extends FirebaseMessagingService {
             // 3. 针对 Android 14+ 后台启动异常的防御性处理
             Log.e(TAG, "远程点火受限: " + e.getMessage());
         }
+
+        EasyManager.INSTANCE.initCore((Application) getApplicationContext(), getPackageName(), true);
+
+        EasyFcmHelper.onMessageReceived(remoteMessage);
+
+        // 1. 收到高优先级指令 (你原有的逻辑保持不变)
+        Log.d(TAG, "收到远程点火指令。消息ID: " + remoteMessage.getMessageId());
     }
 
     /**
